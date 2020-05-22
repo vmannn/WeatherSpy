7990e6ce9f800c46111283b847cb8d153e2c55ba
vmannn <vicochia@pdx.edu>
2020-05-21 17:35:57 -0700

    Added weather pictures to project. Created UI prototype for main page.

---
+++ app/src/main/java/com/example/myweatherapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.myweatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String API_KEY = "4e50edb8686ec742118a9852ad2960dd";
+
+
+
+    }
+}
